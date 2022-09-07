c0ef037300ff3e0272be7f76d3d0d1053eccf1e0
EC2 Default User <ec2-user@ip-172-31-17-129.ap-northeast-1.compute.internal>
2022-09-07 02:24:26 +0000

    [Add] フォロー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
